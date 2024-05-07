7926a0f60c889be94c1f18aa211df24b71da1020
Serhii Levytskyi <slevytskyi@deloitte.com>
2024-05-07 11:23:25 +0300

    Tree: SumOfLEftLEaves + LinkedListInBT (check if exists)

---
+++ zConsole/Codility_exercises/Tree/LinkedListInBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿namespace zConsole.Codility_exercises.Tree
+{
+    public class LinkedListInBinaryTree
+    {
+        public bool IsSubPath(ListNode head, TreeNode root)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (Match(head, root)) return true;
+            return IsSubPath(head, root.left) || IsSubPath(head, root.right);
+        }
+
+        private bool Match(ListNode head, TreeNode root)
+        {
+            if (head == null) return true;
+            if (root == null || root.val != head.val) return false;
+            return Match(head.next, root.left) || Match(head.next, root.right);
+        }
+    }
+}
---
+++ zConsole/Codility_exercises/Tree/SumOfLeftLeaves.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zConsole.Codility_exercises.Tree
+{
+    public class SumOfLeftLeaves
+    {
+        public int GetSumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int sum = 0;
+
+            if (root.left != null)
+            {
+                if (IsLeave(root.left))
+                {
+                    sum += root.left.val;
+                }
+                else
+                {
+                    sum += GetSumOfLeftLeaves(root.left);
+                }
+            }
+            sum += GetSumOfLeftLeaves(root.right);
+            return sum;
+        }
+
+        private bool IsLeave(TreeNode node)
+        {
+            return node.left == null && node.right == null;
+        }
+    }
+}
